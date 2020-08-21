07d4623798f45b0ba875c6553f27a0b75c5d0365
ahmarulabdi <rndmjck@gmail.com>
2020-08-22 05:56:08 +0700

    adding html element lifecycle

--- image-figure.js
+++ image-figure.js
@@ -1,2 +1,24 @@
 class ImageFigure extends HTMLElement {
+    constructor() {
+        super();
+        console.log('constructor image figure');
+    }
+
+    connectedCallback() {
+        console.log('on connected');
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        console.log(`on attr ${name} changed from ${oldValue} into ${newValue}`);
+    }
+
+    adoptedCallback() {
+        console.log('on adopted callback');
+    }
+
+    disconnectedCallback() {
+        console.log('on disconnected callback');
+    }
 }
+
+customElements.define('image-figure', ImageFigure);
