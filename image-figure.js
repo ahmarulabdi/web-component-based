11f60068e2625fb5a233fe0a6216ee8b94761a1d
ahmarulabdi <rndmjck@gmail.com>
2020-08-22 06:06:32 +0700

    adding attr html observer

--- image-figure.js
+++ image-figure.js
@@ -1,24 +1,31 @@
 class ImageFigure extends HTMLElement {
     constructor() {
-        super();
-        console.log('constructor image figure');
+      super();
+      console.log('constructor image figure');
     }
-
+    
     connectedCallback() {
         console.log('on connected');
     }
-
+    
     attributeChangedCallback(name, oldValue, newValue) {
         console.log(`on attr ${name} changed from ${oldValue} into ${newValue}`);
-    }
+      }
 
     adoptedCallback() {
         console.log('on adopted callback');
     }
-
+    
     disconnectedCallback() {
         console.log('on disconnected callback');
     }
-}
-
-customElements.define('image-figure', ImageFigure);
+    // digunakan untuk mengamati perubahan nilai attribute caption
+    /* kita bisa menetapkan lebih dari satu atribut yang diamati.
+       dengan memisahkan nama atribut menggunakan koma. Contoh: */
+    // return ["caption", "title", "src", ...]
+    static get observedAttributes() {
+      return ["caption"];
+    }
+   }
+    
+   customElements.define('image-figure', ImageFigure);
