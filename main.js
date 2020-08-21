333d108b0d92c447a4954bb2dbdfea93c041f891
ahmarulabdi <rndmjck@gmail.com>
2020-08-22 06:07:19 +0700

    adding condition for new element

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-customElements.define('image-figure', ImageFigure);
-
-let imageFigureElement = document.createElement('image-figure');
-document.body.appendChild(imageFigureElement);
+let imageFigureElement = document.querySelector("image-figure");
+     
+// Jika tidak tersedia pada DOM maka dibuat secara sintaksis.
+if (!imageFigureElement) {
+ imageFigureElement = document.createElement("image-figure");
+ document.body.appendChild(imageFigureElement);
+}
