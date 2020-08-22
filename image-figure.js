f063278a66028398ab5a5f2003c542448acc946a
ahmarulabdi <rndmjck@gmail.com>
2020-08-22 07:16:58 +0700

    adding html via html element object

--- image-figure.js
+++ image-figure.js
@@ -13,13 +13,36 @@ class ImageFigure extends HTMLElement {
         this.src = this.getAttribute("src") || null;
         this.alt = this.getAttribute("alt") || null;
         this.caption = this.getAttribute("caption") || null;
-        
+
         this.innerHTML = `
-            <figure>
+        <style>
+            figure {
+            border: thin #c0c0c0 solid;
+            display: flex;
+            flex-flow: column;
+            padding: 5px;
+            max-width: 220px;
+            margin: auto;
+            }
+    
+            figure > img {
+            max-width: 220px;
+            }
+    
+            figure > figcaption {
+            background-color: #222;
+            color: #fff;
+            font: italic smaller sans-serif;
+            padding: 3px;
+            text-align: center;
+            }
+        </style>
+    
+        <figure>
             <img src="${this.src}"
                 alt="${this.alt}">
             <figcaption>${this.caption}</figcaption>
-            </figure>
+        </figure>
         `;
     }
 
